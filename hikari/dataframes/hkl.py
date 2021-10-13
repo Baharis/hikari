07d06785d249735efe08cb8843eccd5cfb4f4d3a
Daniel Tchoń <dantch@onet.eu>
2021-10-13 16:09:46 +0200

    Minor correction to check git connection

--- hikari/dataframes/hkl.py
+++ hikari/dataframes/hkl.py
@@ -264,7 +264,7 @@ class HklKeys:
     defined_keys = {'h', 'k', 'l', 'F', 'I', 'si', 'sf', 'b', 'm', 'la', 'ph',
                     'u', 'r', 't', 'u1', 'u2', 'u3', 'v1', 'v2', 'v3',
                     'x', 'y', 'z', 'ze', 'ze2', 'Iosi', 'Icsi', 'equiv'}
-    #TODO check if needed and cplt?
+    # TODO check if needed and cplt?
 
     def __init__(self, keys=set()):
         # DEFINE ALL KNOWN KEYS
