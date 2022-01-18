ff6c96940bf2504f247f390f4fd42c4bcff60638
Daniel Tchoń <dantch@onet.eu>
2022-01-18 16:02:25 +0100

    Vectorised hikari.utility.math_tools.fibonacci_sphere using numpy

--- hikari/utility/math_tools.py
+++ hikari/utility/math_tools.py
@@ -78,17 +78,15 @@ def fibonacci_sphere(samples=1, seed=1337):
     """
     random.seed(seed)
     rnd = random.random() * samples
-    points = []
     offset = 2. / samples
     increment = np.pi * (3. - np.sqrt(5.))
-    for i in range(samples):
-        y = ((i * offset) - 1) + (offset / 2)
-        r = np.sqrt(1 - pow(y, 2))
-        phi = ((i + rnd) % samples) * increment
-        x = np.cos(phi) * r
-        z = np.sin(phi) * r
-        points.append(np.array([x, y, z]))
-    return np.vstack(points)
+    i = np.arange(0, samples)
+    y = ((i * offset) - 1) + (offset / 2)
+    r = np.sqrt(1 - pow(y, 2))
+    phi = ((i + rnd) % samples) * increment
+    x = np.cos(phi) * r
+    z = np.sin(phi) * r
+    return np.vstack([x, y, z]).T
 
 
 def euler_rodrigues_matrix(a, b, c, d):
