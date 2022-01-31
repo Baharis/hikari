e61a559718db5eaf624855410d1c633d83106b2e
Daniel Tchoń <dantch@onet.eu>
2022-01-31 20:05:17 +0100

    Added atomic_form_factor function to hikari.utility.chem_tools. Import errors still present.

--- hikari/utility/chem_tools.py
+++ hikari/utility/chem_tools.py
@@ -2,6 +2,8 @@
 This file contains all constants and functions containing purely chemical
 information / methods used in the package.
 """
+from numpy import exp as e
+from hikari.resources import Xray_atomic_form_factors
 
 chemical_elements = \
     ('H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
@@ -17,3 +19,10 @@ chemical_elements = \
      'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds',
      'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og')
 """Tuple containing 2-letter symbols of the first 118 chemical elements."""
+
+
+def atomic_form_factor(atom, r):
+    f = Xray_atomic_form_factors.loc[atom]
+    s2 = r**2 / 4
+    return f['a1'] * e(-f['b1'] * s2) + f['a2'] * e(-f['b2'] * s2) + \
+           f['a3'] * e(-f['b3'] * s2) + f['a4'] * e(-f['b4'] * s2) + f['c']
