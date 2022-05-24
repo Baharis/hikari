00f59cbec473626854f92b3e59a4b236801d8777
Daniel Tchoń <dantch@onet.eu>
2022-05-24 20:29:52 +0200

    Reorganised imports in `hikari.utility.__init__` to prevent import errors

--- hikari/utility/__init__.py
+++ hikari/utility/__init__.py
@@ -5,11 +5,11 @@ to the actual contents of the whole library,
 but do significantly improve clarity of the code present in other modules.
 """
 
-from .artists import GnuplotAngularHeatmapArtist
 from .chem_tools import chemical_elements, split_atom_label
-from .math_tools import angle2rad, cartesian2spherical, spherical2cartesian, \
+from .math_tools import angle2rad, cart2sph, sph2cart, \
     fibonacci_sphere, rotation_from, rotation_around, weighted_quantile
 from .list_tools import cubespace, find_best, rescale_list_to_range,\
     rescale_list_to_other
 from .os_tools import make_abspath
 from .palettes import gnuplot_map_palette, mpl_map_palette
+from .artists import GnuplotAngularHeatmapArtist, MatplotlibAngularHeatmapArtist
