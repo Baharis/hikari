890b886f5d7368ae303b53079f642f28c81e75ba
Daniel Tchoń <dantch@onet.eu>
2022-05-26 21:38:40 +0200

    Changed a number of attributes to properties in `AngularPropertyExplorer` (but still not functional)

--- hikari/scripts/angular_explorer.py
+++ hikari/scripts/angular_explorer.py
@@ -52,46 +52,35 @@ class AngularPropertyExplorer:
 
     HKL_IS_READ_NOT_GENERATED = True
 
+    POLAR_LIMIT_DICT = {Group.System.triclinic: Interval(0, 180),
+                        Group.System.monoclinic: Interval(0, 180),
+                        Group.System.orthorhombic: Interval(0, 90),
+                        Group.System.tetragonal: Interval(0, 90),
+                        Group.System.trigonal: Interval(0, 90),
+                        Group.System.hexagonal: Interval(0, 90),
+                        Group.System.cubic: Interval(0, 90)}
+
+    AZIMUTH_LIMIT_DICT = {Group.System.triclinic: Interval(-45, 135),
+                          Group.System.monoclinic: Interval(0, 90),
+                          Group.System.orthorhombic: Interval(0, 90),
+                          Group.System.tetragonal: Interval(0, 90),
+                          Group.System.trigonal: Interval(0, 120),
+                          Group.System.hexagonal: Interval(0, 120),
+                          Group.System.cubic: Interval(0, 90)}
+
     def __init__(self):
-        self.gnu_path = ''
-        self.gnu_png_path = ''
-        self.his_path = ''
-        self.hkl_path = ''
-        self.lst_path = ''
-        self.mpl_png_path = ''
-        self.mesh_path = ''
+        self._path = ''
         self.sg = Group()
-        self.pg = Group()
-        self.lg = Group()
+        self.axis = ''
         self.heat_limits = Interval(0, 1)
         self.hkl_frame = HklFrame()
         self.oa = 0.0
         self.orientation = None
         self.resolution = 1.2
-        self.th_limits = Interval(0, 0)
-        self.ph_limits = Interval(0, 0)
-        self.ax = ''
+        self.axis = ''
         self.fix_scale = False
         self.histogram = False
         self.output_quality = 1
-
-    def set_path(self, path):
-        """
-        Set the path to the workspace. The directory and stem must be common
-        for all input and output files, the extensions are set automatically.
-        :param path: A path to any file with correct stem in working directory.
-        :type path: str
-        """
-        png_path = Path(make_abspath(path))
-        dir_, stem, ext = png_path.parent, png_path.stem, png_path.suffix
-        base_path = str(Path().joinpath(dir_, stem))
-        self.gnu_path = base_path + self.GNUPLOT_INPUT_EXTENSION
-        self.gnu_png_path = base_path + self.GNUPLOT_OUTPUT_EXTENSION
-        self.his_path = base_path + self.HISTOGRAM_EXTENSION
-        self.hkl_path = base_path + self.HKL_EXTENSION
-        self.lst_path = base_path + self.LISTING_EXTENSION
-        self.mpl_png_path = base_path + self.MATPLOTLIB_EXTENSION
-        self.mesh_path = base_path + self.MESH_EXTENSION
 
     def set_experimental(self, opening_angle, orientation, resolution):
         self.oa = opening_angle
@@ -100,10 +89,9 @@ class AngularPropertyExplorer:
 
     def set_hkl_frame(self, a, b, c, al, be, ga, space_group, wavelength, axis):
         self.sg = SG[space_group]
-        self.pg = self.sg.reciprocate()
-        self.lg = self.pg.lauefy()
         self.hkl_frame.edit_cell(a=a, b=b, c=c, al=al, be=be, ga=ga)
         self.hkl_frame.la = wavelength
+        self.axis = axis
         if self.HKL_IS_READ_NOT_GENERATED:
             self._read_hkl_frame(hkl_format='shelx_4')
         else:
@@ -112,19 +100,15 @@ class AngularPropertyExplorer:
         total_reflections = self.hkl_frame.table['equiv'].nunique()
         if total_reflections == 0:
             raise KeyError('Specified part of reciprocal space has zero nodes')
-        self._determine_theta_and_phi_limits()
 
-    def set_options(self, ax, fix_scale, histogram, output_quality):
-        self.ax = ''
-        self.fix_scale = False
-        self.histogram = False
+    def set_options(self, path, fix_scale, histogram, output_quality):
+        self.path = path
+        self.fix_scale = fix_scale
+        self.histogram = histogram
         self.output_quality = output_quality
 
     def explore(self):
-        th_range = self.th_limits.arange(step=self.angle_res)
-        ph_range = self.ph_limits.arange(step=self.angle_res)
-        th_mesh, ph_mesh = self.th_limits.mesh_with(self.ph_limits,
-                                                    step=self.angle_res)
+
         data_dict = {'th': [], 'ph': [], 'cplt': [], 'r1': [], 'weight': []}
 
         self.heat_limits = Interval(
@@ -155,28 +139,20 @@ class AngularPropertyExplorer:
     def _read_hkl_frame(self, hkl_format):
         """Read reflections of hkl which will be cut in further steps"""
         f = self.hkl_frame
-        f.read(hkl_path=self.hkl_path, hkl_format=hkl_format)
+        f.read(hkl_path=self.path + self.HKL_EXTENSION, hkl_format=hkl_format)
         f.trim(limit=min(f.r_lim, self.resolution))
         f.find_equivalents(point_group=self.lg)
         return f
 
-    def _determine_theta_and_phi_limits(self):
-        """Define range of coordinates where potency map will be calculated."""
-        if self.sg.system in {Group.System.triclinic}:
-            self.th_limits = Interval(0, 180)
-            self.limits = Interval(-45, 135)
-        elif self.sg.system in {Group.System.monoclinic}:
-            self.th_limits = Interval(0, 180)
-            self.ph_limits = Interval(0, 90)
-        elif self.sg.system in {Group.System.orthorhombic,
-                                Group.System.tetragonal, Group.System.cubic}:
-            self.th_limits = Interval(0, 90)
-            self.ph_limits = Interval(0, 90)
-        elif self.sg.system in {Group.System.trigonal, Group.System.hexagonal}:
-            self.th_limits = Interval(0, 90)
-            self.ph_limits = Interval(0, 120)
-        else:
-            raise ValueError('Unknown crystal system (trigonal not supported)')
+    @property
+    def th_limits(self):
+        """Interval range of polar angle where property will be calculated"""
+        return self.POLAR_LIMIT_DICT[self.sg.system]
+
+    @property
+    def ph_limits(self):
+        """Interval range of azimuth angle where property will be calculated"""
+        return self.AZIMUTH_LIMIT_DICT[self.sg.system]
 
     def _draw_matplotlib_map(self):
         ma = MatplotlibAngularHeatmapArtist()
@@ -188,7 +164,7 @@ class AngularPropertyExplorer:
         ma.heat_palette = self.axis
         ma.polar_limits = self.th_limits
         ma.azimuth_limits = self.ph_limits
-        ma.plot(self.mpl_png_path)
+        ma.plot(self.path + self.MATPLOTLIB_EXTENSION)
 
     def _draw_gnuplot_map(self):
         ga = GnuplotAngularHeatmapArtist()
@@ -201,7 +177,7 @@ class AngularPropertyExplorer:
         ga.histogram = self.histogram
         ga.polar_limits = self.th_limits
         ga.azimuth_limits = self.ph_limits
-        ga.plot(self.gnu_png_path)
+        ga.plot(self.path + self.GNUPLOT_OUTPUT_EXTENSION)
 
     @property
     def angle_res(self):
@@ -210,19 +186,69 @@ class AngularPropertyExplorer:
         return {1: 15, 2: 10, 3: 5, 4: 2, 5: 1}[self.output_quality]
 
     @property
+    def path(self):
+        """
+        Provides path = directory + stem to the workspace. They are common to
+        all input and output files, the extensions are specified as class var.
+        """
+        return self._path
+
+    @path.setter
+    def path(self, path):
+        abs_path = Path(make_abspath(path))
+        dir_, stem, ext = abs_path.parent, abs_path.stem, abs_path.suffix
+        self._path = str(Path().joinpath(dir_, stem))
+
+    @property
+    def pg(self):
+        return self.sg.reciprocate()
+
+    @property
+    def lg(self):
+        return self.pg.lauefy()
+
+    @property
     def focus(self):
         if self.orientation is None:
             return []
         else:
             _focus = []
+            a = lin.inv(self.orientation) @ np.array((1, 0, 0))
             for op in self.lg.operations:
-                v = self.hkl_frame.A_r.T @ op.tf @ lin.inv(self.orientation) \
-                    @ np.array((1, 0, 0))
-                c = cart2sph(*v)
-                if np.rad2deg(c[1]) in self.th_limits and \
-                        np.rad2deg(c[2]) in self.ph_limits:
+                v = self.hkl_frame.A_r.T @ op.tf @ a
+                c = np.rad2deg(cart2sph(*v))
+                if c[1] in self.th_limits and c[2] in self.ph_limits:
                     _focus.append(v / lin.norm(v))
         return _focus
+
+    @property
+    def th_range(self):
+        return self.th_limits.arange(step=self.angle_res)
+
+    @property
+    def ph_range(self):
+        return self.ph_limits.arange(step=self.angle_res)
+
+    @property
+    def th_mesh(self):
+        return self.th_limits.mesh_with(self.ph_limits, step=self.angle_res)[0]
+
+    @property
+    def ph_mesh(self):
+        return self.th_limits.mesh_with(self.ph_limits, step=self.angle_res)[1]
+
+    def orientation_weight(self, th, ph):
+        """Calculate how much each point should contribute to distribution"""
+        def sphere_cutout_area(th1, th2, ph_span):
+            """Calculate sphere area in specified ph and th degree range.
+            For exact math, see articles about spherical cap and sector."""
+            return np.deg2rad(abs(ph_span)) * \
+                   abs(np.cos(np.deg2rad(th1)) - np.cos(np.deg2rad(th2)))
+        th_max = min(th + self.angle_res / 2., self.th_limits[1])
+        th_min = max(th - self.angle_res / 2., self.th_limits[0])
+        ph_max = min(ph + self.angle_res / 2., self.ph_limits[1])
+        ph_min = max(ph - self.angle_res / 2., self.ph_limits[0])
+        return sphere_cutout_area(th_min, th_max, ph_max-ph_min)
 
 
 class AngularPotencyExplorer(AngularPropertyExplorer):
--- hikari/utility/math_tools.py
+++ hikari/utility/math_tools.py
@@ -48,12 +48,12 @@ def cart2sph(x, y, z):
     :param z: Cartesian coordinates or vector z
     :type z: float or np.ndarray
     :return: Spherical coordinates: radius, polar angle, and azimuth angle
-    :rtype: tuple[float, float, float] or tuple[np.ndarray, np.ndarray, np.ndarray]
+    :rtype: np.ndarray
     """
     r = (x ** 2 + y ** 2 + z ** 2) ** 0.5
     p = np.arccos(z / r)
     a = np.arctan2(y, x)
-    return r, p, a
+    return np.array([r, p, a])
 
 
 def sph2cart(r, p, a):
@@ -67,12 +67,12 @@ def sph2cart(r, p, a):
     :param a: Spherical coordinate or vector azimuth angle
     :type a: float or np.ndarray
     :return: Cartesian coordinates: x, y, and z
-    :rtype: tuple[float, float, float] or tuple[np.ndarray, np.ndarray, np.ndarray]
+    :rtype: np.ndarray
     """
     x = r * np.cos(a) * np.sin(p)
     y = r * np.sin(a) * np.sin(p)
     z = r * np.cos(p)
-    return x, y, z
+    return np.array([x, y, z])
 
 
 def fibonacci_sphere(samples=1, seed=1337):
