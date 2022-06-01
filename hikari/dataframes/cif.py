83756a3c1faf76a53b2bdf16bc5d9a82f9313161
Daniel Tchoń <dantch@onet.eu>
2022-06-01 22:13:08 +0200

    Added docstrings to `hikari.dataframes.CifIO` methods

--- hikari/dataframes/cif.py
+++ hikari/dataframes/cif.py
@@ -74,73 +74,29 @@ class CifIO:
         self.data_block_header = cif_block_header
         self.data = OrderedDict()
 
-    class ReadingState(Enum):
-        default = 0
-        loop = 1
-        loop_header = 2
-        multiline = 3
-
-    def substitute_whitespace_in_quotes(self, string, reverse=False):
-        # see: https://stackoverflow.com/q/46967465/, https://regex101.com/
-        split_string = []
-        matching_quotes_regex = r"""(["'])((?:\\\1|(?:(?!\1)).)*)(\1)"""
-        for i, m in enumerate(re.split(matching_quotes_regex, string)):
-            if i % 4 == 2:
-                for ws, sub in self.WHITESPACE_SUBSTITUTES.items():
-                    m = m.replace(sub, ws) if reverse else m.replace(ws, sub)
-            split_string.append(m)
-        return ''.join(split_string)
-
-    @staticmethod
-    def remove_outer_quotes(s):
-        left = s[0]
-        right = s[-1]
-        return s.strip(left) if left == right and left in ('"', "'") else s
-
-    def locate_block(self, block_header=''):
-        data_block_string = 'data_' + block_header
-        data_block_start = 1
-        data_block_end = None
-        for line_number, line in enumerate(self.file_lines):
-            if data_block_string in line:
-                data_block_start = line_number + 1
-                break
-        for line_number, line in enumerate(self.file_lines[data_block_start:]):
-            if line.startswith('data_'):
-                data_block_end = line_number
-                break
-        return data_block_start, data_block_end
-
-    def read(self):
-        with open(self.file_path, 'r') as cif_file:
-            self.file_lines = cif_file.readlines()
-        block_start, block_end = self.locate_block(self.data_block_header)
-        self.parse_lines(start=block_start, end=block_end)
-        return self.data
-
-    class CifDataBuffer:
-        """
-        This class stores a chunk of cif until it is parsed completely.
-        After the chunk is complete, calling flush() cleans it.
-        """
+    class DataBuffer:
+        """This class buffers data in temporary dict until flush() is called."""
         def __init__(self, target):
             self.names = []
             self.values = []
             self.target = target
 
         def parse(self, word):
+            """Append the word to names or values based on its first char"""
             if word.startswith('_'):
                 self.names.append(word)
             else:
                 self.values.append(word)
 
         def append_to_multiline(self, string):
+            """Add the word to values if they're empty, concatenate otherwise"""
             if self.values:
                 self.values[-1] = self.values[-1] + '\n' + string
             else:
                 self.values.append(string)
 
         def flush(self):
+            """Update the target dict with names and values stored hitherto"""
             d = OrderedDict()
             lv = len(self.values)
             ln = len(self.names)
@@ -156,16 +112,57 @@ class CifIO:
             self.names = []
             self.values = []
 
-    def split_line(self, line):
-        substituted_line = self.substitute_whitespace_in_quotes(line)
-        words = []
-        for word in substituted_line.strip().split():
-            word = self.substitute_whitespace_in_quotes(word, reverse=True)
-            words.append(self.remove_outer_quotes(word))
-        return words
+    class ReadingState(Enum):
+        """This class stores current cif reading state (eg. inside loop etc.)"""
+        default = 0
+        loop = 1
+        loop_header = 2
+        multiline = 3
+
+    @staticmethod
+    def remove_outer_quotes(s):
+        """
+        Return the string without matching outer quotation marks: `'` or `"`
+        :param s: the string which should be stripped
+        :type s: str
+        :return: string `s` without matching outer quotation marks
+        :rtype: str
+        """
+        left = s[0]
+        right = s[-1]
+        return s.strip(left) if left == right and left in ('"', "'") else s
+
+    def locate_block(self, block_header=''):
+        """
+        Determine the numbers of first lines of the seeked and next cif block
+        :param block_header: name of the cif block which should be localised
+        :type block_header: str
+        :return: tuple with numbers of 1st-line-to-read and 1st-line-not-to-read
+        :rtype: tuple
+        """
+        data_block_string = 'data_' + block_header
+        data_block_start = 1
+        data_block_end = None
+        for line_number, line in enumerate(self.file_lines):
+            if data_block_string in line:
+                data_block_start = line_number + 1
+                break
+        for line_number, line in enumerate(self.file_lines[data_block_start:]):
+            if line.startswith('data_'):
+                data_block_end = line_number
+                break
+        return data_block_start, data_block_end
 
     def parse_lines(self, start, end):
-        buffer = self.CifDataBuffer(target=self.data)
+        """
+        Read the data from :attr:`~.CifIO.lines` numbered `start` to `end`,
+        interpret it, and store it in :attr:`~.CifIO.data` dictionary.
+        :param start: number of the first line which data should be read from
+        :type start: int
+        :param end: number of the first line which should not be read anymore
+        :type end: int
+        """
+        buffer = self.DataBuffer(target=self.data)
         state = self.ReadingState.default
         for line in self.file_lines[start:end]:
             if state is self.ReadingState.loop_header:
@@ -200,6 +197,54 @@ class CifIO:
             if not words and state is self.ReadingState.loop:
                 pass
         buffer.flush()
+
+    def read(self):
+        """
+        Read the contents of cif currently pointed by :attr:`~.CifIO.file_path`
+        and block :attr:`~.CifIO.data_block_header` and return them to a dict.
+        :return: A dictionary containing information read from .cif file.
+        :rtype: dict
+        """
+        with open(self.file_path, 'r') as cif_file:
+            self.file_lines = cif_file.read().splitlines()
+        block_start, block_end = self.locate_block(self.data_block_header)
+        self.parse_lines(start=block_start, end=block_end)
+        return self.data
+
+    def split_line(self, line):
+        """
+        Split line into words, keeping words inside quotation marks together.
+        :param line: line to be split based on whitespace into words
+        :type line: str
+        :return: list of words obtained from splitting
+        :rtype: list
+        """
+        substituted_line = self.substitute_whitespace_in_quotes(line)
+        words = []
+        for word in substituted_line.strip().split():
+            word = self.substitute_whitespace_in_quotes(word, reverse=True)
+            words.append(self.remove_outer_quotes(word))
+        return words
+
+    def substitute_whitespace_in_quotes(self, string, reverse=False):
+        """
+        Substitute whitespace between matching quotation marks with substitutes.
+        :param string: text in which whitespace will be substituted
+        :type string: str
+        :param reverse: if True, change the substitutes back to whitespace
+        :type reverse: bool
+        :return: string where whitespace/substitutes inside quotes were changed
+        :rtype: str
+        """
+        # see: https://stackoverflow.com/q/46967465/, https://regex101.com/
+        split_string = []
+        matching_quotes_regex = r"""(["'])((?:\\\1|(?:(?!\1)).)*)(\1)"""
+        for i, m in enumerate(re.split(matching_quotes_regex, string)):
+            if i % 4 == 2:
+                for ws, sub in self.WHITESPACE_SUBSTITUTES.items():
+                    m = m.replace(sub, ws) if reverse else m.replace(ws, sub)
+            split_string.append(m)
+        return ''.join(split_string)
 
 
 if __name__ == '__main__':
