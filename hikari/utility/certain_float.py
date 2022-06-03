edd2f518c3bcafa7f68c21d7dfcca6fd76b2ed7a
Daniel Tchoń <dantch@onet.eu>
2022-06-03 17:03:46 +0200

    Defined `cfloat` "type" (str2ufloat2float converter) to `hikari.utility`

---
+++ hikari/utility/certain_float.py
@@ -0,0 +1,13 @@
+import uncertainties
+
+
+def cfloat(string):
+    """
+    Create "certain float" (`ufloat.n`) from string by converting it first
+    to `uncertainties.ufloat` and then taking the nominal value
+    :param string: string to be converted
+    :type string: str
+    :return: float with no uncertainty
+    :rtype: float
+    """
+    return uncertainties.ufloat_fromstr(representation=string)
