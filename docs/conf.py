a1f868be97511e7b2cb75fae2d1a0f85a20f9a20
Daniel Tchoń <dantch@onet.eu>
2022-06-10 16:08:41 +0200

    Bumped `hikari` version to 0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = u'2022, Daniel Tchoń'
 author = u'Daniel Tchoń'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with io.open(os.path.join(HERE, "README.md"), encoding="utf-8") as f:
 
 setup(
     name='hikari-toolkit',
-    version='0.2.0',
+    version='0.2.1',
     author='Daniel Tchoń',
     author_email='dtchon@chem.uw.edu.pl',
     packages=find_packages(exclude=('legacy', )),
