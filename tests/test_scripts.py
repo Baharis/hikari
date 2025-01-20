6b98f109f5bd6228158e014f851bcc836334fcfb
Daniel Tchoń <dantch@onet.eu>
2025-01-20 18:32:58 +0100

    :bug: Fix test which wrongly assumed that diagonal 3-fold screw axes should extinct -hhh, h-hh, and hh-h reflections

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -101,8 +101,7 @@ class TestHklScripts(unittest.TestCase):
     def test_completeness_statistics(self):
         kwargs = dict({'space_group': 'Fm-3m'}, **nacl_commons)
         stdout = self.get_stdout(completeness_statistics, kwargs)
-        print(stdout)
-        line = '(2.373, 2.468]    287      6       6  11.039363'
+        line = '(2.373, 2.468]    287      6       6  11.039363   1.0   47.833333'
         self.assertIn(line, stdout)
         # TODO: for some reason, on ubuntu latest ONLY, one refl is not read?
         # TODO: check that out eventually - the last reflection not read
